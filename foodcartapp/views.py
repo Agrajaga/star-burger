6d85a8feb218d1f57acc858faebb666acc447406
Michael <soplomiha@gmail.com>
2022-06-30 01:07:44 +0300

    Extend data validation for def register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from phonenumbers import is_valid_number, parse
 
 from .models import Order, OrderItem, Product
 
@@ -58,17 +59,49 @@ def product_list_api(request):
     })
 
 
+def validate_field(data: dict, name: str, field_type: type) -> str | None:
+    try:
+        if data[name] is None:
+            return f'{name}: Поле не может быть пустым'
+        if not isinstance(data[name], field_type):
+            return f'{name}: Ожидался тип поля {field_type}'
+        if not data[name]:
+            return f'{name}: Поле не может быть пустым'
+    except KeyError:
+        return f'{name}: Обязательное поле'
+
+
 @api_view(['POST'])
 def register_order(request):
     order_data = request.data
     if not order_data:
-        return Response()
-    if 'products' not in order_data:
-        return Response(['products: Обязательное поле'], status=400)
-    if not isinstance(order_data['products'], list):
-        return Response(['products: Ожидался list со значениями'], status=400)
-    if not order_data['products']:
-        return Response(['products: Список не может быть пустым'], status=400)
+        return Response(['Пустой запрос'], status=400)
+    errors = []
+    error = validate_field(order_data, 'firstname', str)
+    if error:
+        errors.append(error)
+    error = validate_field(order_data, 'lastname', str)
+    if error:
+        errors.append(error)
+    error = validate_field(order_data, 'phonenumber', str)
+    if error:
+        errors.append(error)
+    elif not is_valid_number(parse(order_data['phonenumber'])):
+        errors.append('phonenumber: Введен некорректный номер телефона')
+    error = validate_field(order_data, 'address', str)
+    if error:
+        errors.append(error)
+    error = validate_field(order_data, 'products', list)
+    if error:
+        errors.append(error)
+    else:
+        order_ids = set(item['product'] for item in order_data['products'])
+        db_ids = Product.objects.all().values_list('id', flat=True)
+        err_ids = order_ids.difference(db_ids)
+        for err_id in err_ids:
+            errors.append(f'products: Недопустимый первичный ключ {err_id}')
+    if errors:
+        return Response(errors, status=400)
 
     new_order = Order.objects.create(
         first_name=order_data['firstname'],
