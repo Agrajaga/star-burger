ac686ffffd1f476235bc6e0a8602a632391dbad5
Michael <soplomiha@gmail.com>
2022-06-30 23:04:54 +0300

    Rewrite data validation logic with DRF

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -125,26 +125,23 @@ class RestaurantMenuItem(models.Model):
 
 
 class Order(models.Model):
-    first_name = models.CharField(
+    firstname = models.CharField(
         'имя',
         max_length=50,
-        blank=True,
         db_index=True,
     )
-    last_name = models.CharField(
+    lastname = models.CharField(
         'фамилия',
         max_length=50,
-        blank=True,
         db_index=True,
     )
-    phone = PhoneNumberField(
+    phonenumber = PhoneNumberField(
         'телефон',
         db_index=True
     )
     address = models.CharField(
         'адрес',
         max_length=150,
-        blank=True,
         db_index=True,
     )
 
@@ -153,13 +150,13 @@ class Order(models.Model):
         verbose_name_plural = 'заказы'
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name} {self.address}'
+        return f'{self.firstname} {self.lastname} {self.address}'
 
 
 class OrderItem(models.Model):
     order = models.ForeignKey(
         Order,
-        related_name='items',
+        related_name='products',
         verbose_name='заказ',
         on_delete=models.CASCADE,
     )
