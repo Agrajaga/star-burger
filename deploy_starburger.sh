8cfe2be412ca8afbdefd38ef9aaf9649286650ec
Michael <soplomiha@gmail.com>
2022-11-05 00:42:59 +0300

    Add deploy script

---
+++ deploy_starburger.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+echo "Update repo from GitHub..."
+git pull
+echo "Install Python requrements..."
+source starburger_venv/bin/activate
+pip install -r requirements.txt
+echo "Install JS requirements..."
+npm ci
+echo "Build JS modules..."
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+echo "Collect static files..."
+python manage.py collectstatic
+echo "Prepare database..."
+python manage.py migrate
+echo "Restart services..."
+sudo systemctl restart starburger
+sudo systemctl restart nginx
+echo "Success!"
